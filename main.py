dd649883d87082dfc594b50ed9c00dc8b0355e09
Jacob Molyneux <jacobmolyneux@Jacobs-MacBook-Pro.local>
2022-01-26 22:36:19 -0500

    fixed error wher missing data points in a table would crash program. Program now works

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+
 import time
 import csv
 
@@ -9,7 +12,7 @@ driver = webdriver.Chrome('/Users/jacobmolyneux/Desktop/chromedriver')
 username = 'jacobmolyneux2@gmail.com'
 password = 'Jacob#1!'
 print('starting scrape.....')
-driver.get("https://www.linkedin.com/sales/lists/people/6891365935756709888?sortCriteria=CREATED_TIME&sortOrder=DESCENDING")
+driver.get("https://www.linkedin.com/sales/lists/people/6892306035382521856?sortCriteria=CREATED_TIME&sortOrder=DESCENDING")
 print('waiting for page to load')
 time.sleep(6)
     # Store iframe web element
@@ -52,43 +55,63 @@ location_list = []
 
 # for i in driver.find_elements(By.TAG_NAME, 'tr'):
 def scrape_leads():
-    names = driver.find_elements_by_css_selector('[data-anonymize=person-name]')
-    titles = driver.find_elements_by_css_selector('[data-anonymize=job-title]')
-    companies = driver.find_elements_by_css_selector('[data-anonymize=company-name]')
-    locations = driver.find_elements_by_css_selector('[data-anonymize=location]')
-    for i in range(len(names)):
-        print(names[i].get_attribute('href'))
+    table = driver.find_element(By.TAG_NAME, 'tbody')
+    table_rows = table.find_elements(By.TAG_NAME, 'tr')
+    
+    print(len(table_rows))
+    for row in table_rows:
+        name = row.find_element(By.CSS_SELECTOR, '[data-anonymize=person-name]').text
+        #try to get the company
+        try:
+            company = row.find_element(By.CSS_SELECTOR,'[data-anonymize=company-name]').text
+        except NoSuchElementException:
+            company = 'N/A'
+            pass
+        #try to get the job title
+        try:
+            title = row.find_element(By.CSS_SELECTOR,'[data-anonymize=job-title]' ).text
+        except NoSuchElementException:
+            title = 'N/A'
+            pass
+        # try to get the location
+        try:
+            location = row.find_element(By.CSS_SELECTOR, '[data-anonymize=location]').text
+        except NoSuchElementException:
+            title = 'N/A'
+            pass
+        try:
+            linkedIn = row.find_element(By.CSS_SELECTOR, '[data-anonymize=person-name]' ).get_attribute('href')
+        except NoSuchElementException:
+            linkedIn = "N/A"
+            pass
+        leads.append({"Company": company, "Name": name, "Title": title, "Phone Number": '', "Location": location, "LinkedIn": linkedIn})
 
-        
-#     for i in range(len(names)):
-#         leads.append({"Company": companies[i].text, "Name": names[i].text, "Title": titles[i].text,"Phone Number": '', "Location": locations[i].text, "LinkedIn": f"https://linkedin.com/{links[i]}"})
-# print('scraping page number 1')
-# scrape_leads()
-# print('page 1 scraped')
-# print(leads)
-
-# time.sleep(3)
-
-# next_button = driver.find_element_by_css_selector('[aria-label=Next]')
-# next_button.click()
-# time.sleep(1)
-# print('scraping page 2')
-# scrape_leads()
-# print('scraped page number 2')
-
-
-# print(leads)
-
-# field_names = ['Company', 'Name', 'Title', "Phone Number", "Location", "LinkedIn"]
-# with open('/Users/jacobmolyneux/Desktop/new_Leads.csv', 'w') as csvfile:
-#     writer = csv.DictWriter(csvfile, fieldnames=field_names)
-#     writer.writeheader()
-#     writer.writerows(leads)
-
-print('beginning scrape')
+    
+print('scraping page')
 scrape_leads()
+print('page scraped')
+time.sleep(3)
+next_button = driver.find_element_by_css_selector('[aria-label=Next]')
+next_button.click()
+time.sleep(3)
 
-print('scrape over')
+print('scraping page')
+scrape_leads()
+print('page scraped')
+time.sleep(3)
+next_button = driver.find_element_by_css_selector('[aria-label=Next]')
+next_button.click()
+time.sleep(3)
+
+print('scraping page')
+scrape_leads()
+print('page scraped')
+time.sleep(3)
 
 
 
+field_names = ['Company', 'Name', 'Title', "Phone Number", "Location", "LinkedIn"]
+with open('/Users/jacobmolyneux/Desktop/MST_Leads.csv', 'w') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=field_names)
+    writer.writeheader()
+    writer.writerows(leads)
