10d762f99b125263feff8cf2ed54ee3a5a33bdc0
Jacob Molyneux <jacobmolyneux@Jacobs-MacBook-Pro.local>
2022-01-31 13:14:25 -0500

    added an api endpointthat allows the transfer of user details for the scrape

--- server/main.py
+++ server/main.py
@@ -1,9 +1,21 @@
-from flask import Flask
-from flask import request
+from flask import Flask, jsonify, request
+from itsdangerous import json
 
-app = (__name__)
+app = Flask(__name__)
 
-@app.route('/', methods = 'POST', 'GET')
-def get_leads():
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    if (request.method == 'POST'):
+        data = request.get_json()
+        email = data['email']
+        username = data['username']
+        password = data['password']
+        linkedInLink = data['leadListLink']
+        
+        return jsonify({"The Email is:": email, 'The Password is: ': password, 'The Username is: ': username, "The Lead Link is: ": linkedInLink})
+        
+    else:
+        return jsonify({"about":"hello world"})
 
-    
+if __name__ == '__main__':
+    app.run(debug =True)  
